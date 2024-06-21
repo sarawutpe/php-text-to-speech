a590871ba143fbc428a285b0aa7833ccb9f67358
Sarawut Pe <85542134+sarawutpe@users.noreply.github.com>
2024-06-22 03:49:55 +0700

    func to class

--- index.php
+++ index.php
@@ -15,81 +15,129 @@ use Google\Cloud\TextToSpeech\V1\VoiceSelectionParams;
 use Google\Cloud\TextToSpeech\V1\AudioConfig;
 use Google\Cloud\TextToSpeech\V1\AudioEncoding;
 
-function getVoiceFromCache($text)
+class api_v1
 {
-    if (!$text) return '';
-    $path = "files/com_voice/" . md5($text) . ".mp3";
-    if (file_exists($path)) {
-        return $path;
+    function __construct()
+    {
+        $this->index();
     }
-    return '';
-}
 
-function getTextToSpeech()
-{
-    try {
-        // Headers
-        $text = trim($_POST['text']);
-        $text = preg_replace('/\s+/', '', $text);
+    public function index()
+    {
+        $this->getTextToSpeech();
+    }
 
-        if (empty($text)) {
-            $responseArray = array(
-                "status" => "error",
-                "message" => 'Text is required.'
-            );
-            echo json_encode($responseArray);
-            exit();
+    private function getVoiceFromCache($text)
+    {
+        if (!$text) return '';
+        $path = "files/com_voice/" . md5($text) . ".mp3";
+        if (file_exists($path)) {
+            return $path;
         }
+        return '';
+    }
 
-        // Get audio from cache
-        $audio_cache = getVoiceFromCache($text);
-        if (!empty($audio_cache)) {
+    private function getTextToSpeech()
+    {
+        try {
+            // Headers
+            $text = trim($_POST['text']);
+            $text = preg_replace('/\s+/', '', $text);
+
+            if (empty($text)) {
+                $responseArray = array(
+                    "status" => "error",
+                    "message" => 'Text is required.'
+                );
+                echo json_encode($responseArray);
+                exit();
+            }
+
+            // Get audio from cache
+            $audio_cache = $this->getVoiceFromCache($text);
+            if (!empty($audio_cache)) {
+                $responseArray = array(
+                    "status" => "success",
+                    "voice_url" => $audio_cache
+                );
+                echo json_encode($responseArray);
+                exit();
+            }
+
+            // Set the path to your service account credentials JSON file
+            putenv('GOOGLE_APPLICATION_CREDENTIALS=' . __DIR__ . '/credentials/service_account_credentials.json');
+
+            $textToSpeechClient = new TextToSpeechClient();
+
+            // Set the text input to be synthesized
+            $input = new SynthesisInput();
+            $input->setText($text);
+
+            // Build the voice request
+            $voice = new VoiceSelectionParams();
+            $voice->setLanguageCode('th-TH');
+            $voice->setName('th-TH-Standard-A');
+            $audioConfig = new AudioConfig();
+            $audioConfig->setAudioEncoding(AudioEncoding::MP3);
+
+            // Get audio content
+            $response = $textToSpeechClient->synthesizeSpeech($input, $voice, $audioConfig);
+            $audioContent = $response->getAudioContent();
+            $audioFilePath = 'files/com_voice/' . md5($text) . '.mp3';
+            file_put_contents($audioFilePath, $audioContent);
+
+            // Response
             $responseArray = array(
                 "status" => "success",
-                "voice_url" => $audio_cache
+                'message' => 'Audio content generated successfully.',
+                "voice_url" => $audioFilePath
             );
             echo json_encode($responseArray);
             exit();
+        } catch (Exception $e) {
+            http_response_code(400);
+            $responseArray = array("status" => "error", "message" => $e->getMessage());
+            echo json_encode($responseArray);
+            exit();
+        } finally {
+            $textToSpeechClient->close();
+            $this->logger("Handle", "Request");
+        }
+    }
+
+    private function logger($message, $prefix = '')
+    {
+        if (empty($message)) {
+            return;
         }
 
-        // Set the path to your service account credentials JSON file
-        putenv('GOOGLE_APPLICATION_CREDENTIALS=' . __DIR__ . '/credentials/service_account_credentials.json');
+        $logFile = 'files/com_voice/logger.txt';
+        $timestamp = date('Y-m-d H:i:s');
+        $logMessage = "[$timestamp] $prefix: $message\n";
 
-        $textToSpeechClient = new TextToSpeechClient();
+        try {
+            // Check if the file exists, create it if it doesn't
+            if (!file_exists($logFile)) {
+                $fileHandle = fopen($logFile, 'w');
+                if (!$fileHandle) {
+                    throw new Exception("Unable to create log file '$logFile'");
+                }
+                $initMessage = "[$timestamp] File created and initialized.\n";
+                fwrite($fileHandle, $initMessage);
+                fclose($fileHandle);
+            }
 
-        // Set the text input to be synthesized
-        $input = new SynthesisInput();
-        $input->setText($text);
-
-        // Build the voice request
-        $voice = new VoiceSelectionParams();
-        $voice->setLanguageCode('th-TH');
-        $voice->setName('th-TH-Standard-A');
-        $audioConfig = new AudioConfig();
-        $audioConfig->setAudioEncoding(AudioEncoding::MP3);
-
-        // Get audio content
-        $response = $textToSpeechClient->synthesizeSpeech($input, $voice, $audioConfig);
-        $audioContent = $response->getAudioContent();
-        $audioFilePath = 'files/com_voice/' . md5($text) . '.mp3';
-        file_put_contents($audioFilePath, $audioContent);
-
-        // Response
-        $responseArray = array(
-            "status" => "success",
-            'message' => 'Audio content generated successfully.',
-            "voice_url" => $audioFilePath
-        );
-        echo json_encode($responseArray);
-        exit();
-    } catch (Exception $e) {
-        http_response_code(400);
-        $responseArray = array("status" => "error", "message" => $e->getMessage());
-        echo json_encode($responseArray);
-        exit();
-    } finally {
-        $textToSpeechClient->close();
+            // Append the log message to the file
+            $fileHandle = fopen($logFile, 'a');
+            if (!$fileHandle) {
+                throw new Exception("Unable to open log file '$logFile' for appending");
+            }
+            fwrite($fileHandle, $logMessage);
+            fclose($fileHandle);
+        } catch (Exception $e) {
+            echo 'Error: ' . $e->getMessage() . "\n";
+        }
     }
 }
 
-getTextToSpeech();
+$api = new api_v1();
